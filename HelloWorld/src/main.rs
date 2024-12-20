eee7f4d47e137376064d874227df51b637674aa2
gokuraku210 <laptop.worker.d.517@gmail.com>
2024-12-21 00:29:06 +0900

    Setup 2D camera at startup

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -3,5 +3,11 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins) // 基本的なプラグインを追加
+        .add_systems(Startup, setup) // 初期化のスケジュールにシステムを登録
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    // カメラを配置
+    commands.spawn(Camera2d::default());
 }
