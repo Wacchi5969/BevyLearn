0646265539b15aadf0a156fe7acda0efcaf668c2
gokuraku210 <laptop.worker.d.517@gmail.com>
2024-12-23 23:49:53 +0900

    Draw hello bevy

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -10,4 +10,33 @@ fn main() {
 fn setup(mut commands: Commands) {
     // カメラを配置
     commands.spawn(Camera2d::default());
+
+    // テキストを作成
+    commands
+        .spawn((
+            // センタリング指定のノードを作成
+            Node {
+                align_self: AlignSelf::Center,
+                justify_self: JustifySelf::Center,
+                align_content: AlignContent::Center,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                justify_items: JustifyItems::Center,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                ..default()
+            },
+            BackgroundColor(Color::srgb(0.5, 0.0, 0.0)),
+        ))
+        .with_children(|parent| {
+            // テキストを親ノードに配置
+            parent.spawn((
+                Text::new("Hello Bevy!"),
+                TextFont {
+                    font_size: 60.0,
+                    ..default()
+                },
+                TextColor::WHITE,
+            ));
+        });
 }
