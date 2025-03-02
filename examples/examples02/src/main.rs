fbd832434b79b9383ddef217e5a30f416fcefb68
wacchi <laptop.worker.d.517@gmail.com>
2025-03-03 00:31:56 +0900

    Implement the ball movement system

--- examples/examples02/src/main.rs
+++ examples/examples02/src/main.rs
@@ -1,6 +1,15 @@
+use bevy::math::vec2;
 use bevy::prelude::*;
 use bevy::window::{PresentMode, WindowTheme};
 use std::env;
+
+const VELOCITY_X: f32 = 100.0;
+
+#[derive(Component)]
+enum Direction {
+    Right,
+    Left,
+}
 
 fn main() {
     // アセットのルートディレクトリを設定
@@ -9,6 +18,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(create_window_plugin())) // 基本的なプラグインを追加
         .insert_resource(Time::<Fixed>::from_seconds(1.0)) // RunFixedMainLoopの更新間隔を設定
         .add_systems(Startup, setup) // 初期化のスケジュールにシステムを登録
+        .add_systems(Update, update_sprite_position)
         .run();
 }
 
@@ -37,8 +47,23 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // カメラを配置
     commands.spawn(Camera2d::default());
 
+    // ボールの生成
     commands.spawn((
-        Sprite::from_image(asset_server.load("images/icon.png")),
-        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+        Sprite {
+            image: asset_server.load("images/icon.png"),
+            custom_size: Some(vec2(100., 100.)),
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        Direction::Right,
     ));
+}
+
+fn update_sprite_position(time: Res<Time>, mut sprite: Query<(&Direction, &mut Transform)>) {
+    for (direction, mut transform) in &mut sprite {
+        match direction {
+            Direction::Right => transform.translation.x += VELOCITY_X * time.delta_secs(),
+            Direction::Left => transform.translation.x -= VELOCITY_X * time.delta_secs(),
+        }
+    }
 }
